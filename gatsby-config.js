f2311dcff08121428917a56f80e834eceefb5014
andrea bredice <bredice.andrea@gmail.com>
2020-08-21 14:53:08 +0200

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,13 @@ module.exports = {
         icon: "src/images/logo.png",
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-162480204-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
